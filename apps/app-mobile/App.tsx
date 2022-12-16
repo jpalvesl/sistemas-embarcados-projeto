02a6015e62244f4f70d7d758ac94c05909fe0503
João Lima <jpalvesl101@gmail.com>
2022-12-16 14:45:12 -0300

    conectando no mqtt via socket

--- apps/app-mobile/App.tsx
+++ apps/app-mobile/App.tsx
@@ -1,9 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Paho from 'paho-mqtt'
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const client = new Paho.Client(
+  "broker.hivemq.com",
+  8000,
+  "jpalvesl"
+);
 
 export default function App() {
+  const [value, setValue] = useState(0);
+
+  function onMessage(message: any) {
+    if (message.destinationName === "mqtt-async-test/value") {
+      setValue(Number(message.payloadString))
+    }
+  }
+
+  function changeValue(client: Paho.Client) {
+    const message = new Paho.Message((value+1).toString())
+    message.destinationName = "mqtt-async-test/value"
+    client.send(message);
+  }
+
+  useEffect(() => {
+    client.connect({
+      onSuccess: () => {
+        console.log("Conectou!");
+        client.subscribe("mqtt-async-test/value")
+        client.onMessageArrived = onMessage;
+      },
+      onFailure: () => {
+        console.log("Falha ao se conectar! :c");
+      }
+    })
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Value is: {value}</Text>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => changeValue(client)}
+      >
+        <Text>Oi teste</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,4 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 150,
+    height: 150,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
