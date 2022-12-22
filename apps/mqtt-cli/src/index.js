3e49ae35a183952f8f3829c8be98ed63a11b11f7
Myrlla Lucas Pereira <myrllajucas20@gmail.com>
2022-12-22 03:02:55 +0000

    Adicionando mensagem de alarme quando estiver ativado

--- apps/mqtt-cli/src/index.js
+++ apps/mqtt-cli/src/index.js
@@ -4,21 +4,39 @@ import mqtt from 'mqtt'
 import readline from 'readline'
 
 const client = mqtt.connect('mqtt://mqtt.eclipseprojects.io')
-const url = "/embarcados/info"
+const urlInfo = "/embarcados/info"
+const urlAlarme = "/embarcados/alarme_acionado"
+var red = '\u001b[31m';
+var reset = '\u001b[0m';
 
 client.on("connect", () => {
     console.info("Conectado com sucesso");
-    client.subscribe([url],()=>{})
+    client.subscribe([urlInfo],()=>{})
+    client.subscribe([urlAlarme], () =>{})
 })
 
-client.on("message", (urlLuz,payload) => {
-    let lampada
-    let alarme
-    const informacao = JSON.parse(payload.toString())
-    informacao.is_luz_ligada? lampada = "Ligada" : lampada = "Desligada"
-    informacao.is_alarme_ligado? alarme= "Ligado" : alarme = "Desligado"
+client.on("message", (url,payload) => {
+    if (url === urlInfo){
+        let lampada
+        let alarme
+        const informacao = JSON.parse(payload.toString())
+        informacao.is_luz_ligada? lampada = "Ligada" : lampada = "Desligada"
+        informacao.is_alarme_ligado? alarme= "Ligado" : alarme = "Desligado"
+
+        console.log("\n--------------------")
+        
+        console.log(`\nTemperatura: ${informacao.temperatura}\nLampada: ${lampada}\nAlarme: ${alarme}\nUmidade: ${informacao.umidade}`)
+
+        console.log("\n--------------------")
+
+    }
+    else if (url === urlAlarme){
+        console.log(red + ` 
+        ░█▀▀█ ▒█░░░ ░█▀▀█ ▒█▀▀█ ▒█▀▄▀█ ▒█▀▀▀ 　 ░█▀▀█ ▀▀█▀▀ ▀█▀ ▒█░░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ 
+        ▒█▄▄█ ▒█░░░ ▒█▄▄█ ▒█▄▄▀ ▒█▒█▒█ ▒█▀▀▀ 　 ▒█▄▄█ ░▒█░░ ▒█░ ░▒█▒█░ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ 
+        ▒█░▒█ ▒█▄▄█ ▒█░▒█ ▒█░▒█ ▒█░░▒█ ▒█▄▄▄ 　 ▒█░▒█ ░▒█░░ ▄█▄ ░░▀▄▀░ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█`+ reset)
+    }
     
-    console.log(`\nTemperatura: ${informacao.temperatura}\n Lampada: ${lampada}\n Alarme: ${alarme}\n Umidade: ${informacao.umidade}`)
 })
 
 const lerLinha = readline.createInterface({
@@ -31,7 +49,7 @@ const lerLinha = readline.createInterface({
 function publica() {
     lerLinha.question("\n\nEscolha uma opção\n 1- Lâmpada\n 2- Alarme\n",comando=>{
         if (comando === "1"){
-        lerLinha.question("\n O que você deseja fazer?\n 0- Desligar luz\n 1- Ligar luz\n",valor => {
+        lerLinha.question("\n O que você deseja fazer?\n 0- Desligar luz\n 1- Ligar luz\n\n",valor => {
             client.publish('/embarcados/luz',valor.toString())
 
 
